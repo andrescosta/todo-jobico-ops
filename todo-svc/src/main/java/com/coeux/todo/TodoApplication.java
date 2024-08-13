2b43282a5e7ce2d42009778d7966c04d578a9af2
Andres <andres.costa@gmail.com>
2024-08-13 17:41:49 -0400

    p

--- todo-svc/src/main/java/com/coeux/todo/TodoApplication.java
+++ todo-svc/src/main/java/com/coeux/todo/TodoApplication.java
@@ -8,7 +8,7 @@ public class TodoApplication {
 
 	
 	public static void main(String[] args) {
-        System.out.println("v7");
+        System.out.println("v8");
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
