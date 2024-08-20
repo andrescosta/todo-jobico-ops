13c2b7064bd79ce09d6da27880b40bebdcb1f7c6
builder <builder@git>
2024-08-20 11:14:17 -0400

    l

--- _infra/bin/setup-argo-helm.sh
+++ _infra/bin/setup-argo-helm.sh
@@ -1,6 +1,3 @@
-version="1.1.$(git rev-list HEAD --count --no-merges)"
-#./build-push-svc.sh $version 
-#./build-push-web.sh $version
 kubectl create secret generic github-token --from-file=../id/token_git_hub.private
 kubectl apply -f ../argocd/jobs.yaml
 kubectl apply -f ../argocd/apps.yaml -n argocd
