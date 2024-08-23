ce0f2cda4d632d22d8f1e2f41dd5e4b1eff91acb
builder <builder@git>
2024-08-23 13:19:16 -0400

    l

--- todo-svc/src/main/java/com/coeux/todo/TodoApplication.java
+++ todo-svc/src/main/java/com/coeux/todo/TodoApplication.java
@@ -8,7 +8,7 @@ public class TodoApplication {
 
 	
 	public static void main(String[] args) {
-        System.out.println("v15");
+        System.out.println("v17");
 		SpringApplication.run(TodoApplication.class, args);
 	}
 
