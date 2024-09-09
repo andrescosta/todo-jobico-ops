ce2f61b8e696d567c7be1da2aad712601db1ad04
builder <builder@git>
2024-09-09 11:30:13 -0400

    deleted

--- _infra/helm/local/install.sh
+++
@@ -1,7 +0,0 @@
-pushd ../
-version=$(date +"%Y%m%d%H%M%S")
-./build-push-svc.sh $version
-./build-push-web.sh $version
-helm install todosvc ../helm/charts/todo-svc-jobico/ --set=image.tag=$version
-helm install todoweb ../helm/charts/todo-web-jobico/ --set=image.tag=$version
-popd
